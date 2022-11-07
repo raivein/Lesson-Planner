17559ec708d60ebccdf2898ea7365cdd4c31da30
ArnauldAnin23 <106489847+Kazz23@users.noreply.github.com>
2022-11-08 01:03:17 +0800

    Arnauld Anin Pushing for updated Update controller bug fix

--- Controller/FileController/UpdateFileController.js
+++ Controller/FileController/UpdateFileController.js
@@ -14,36 +14,36 @@ const LessonFileDB = {
 const UpdateFileController = async(req, res) =>{
     
     //Creating a variable Profs with a function to find for professors ID
-    const Lessons = LessonFileDB.Lesson.find( Lesson => Lesson.id === parseInt(req.body.id));
+    const FoundLesson = LessonFileDB.Lesson.find( Lesson => Lesson.id === parseInt(req.body.id));
 
     //if Profs not found
-    if(!Lessons){
+    if(!FoundLesson){
         
         //send status 400 error and send message 
         return res.status(400).json({"message":`Lesson ID ${req.body.id} not found`});
     }
     //====================================================================================================
     //request input for data below to change and to be updated
-    if (req.body.GradingPeriod) Lessons.GradingPeriod = req.body.GradingPeriod;
-    if (req.body.Week) Lessons.Week = req.body.Week;
-    if (req.body.NoOfHours) Lessons.Week = req.body.NoOfHours;
-    if (req.body.Laboratory) Lessons.Laboratory = req.body.Laboratory;
-    if (req.body.Lecture) Lessons.Lecture = req.body.Lecture;
-    if (req.body.Title) Lessons.LessonInfo.Title = req.body.Title;
-    if (req.body.IntendedLearningOutcomes) Lessons.LessonInfo.InstructionalMaterials = req.body.IntendedLearningOutcomes;
-    if (req.body.Topics) Lessons.LessonInfo.Topics = req.body.Topics;
-    if (req.body.TeachingandLearningActivities) Lessons.LessonInfo.TLAandATs.TLA.TeachingandLearningActivities = req.body.TeachingandLearningActivities;
-    if (req.body.AssessementTasks) Lessons.LessonInfo.TLAandATs.ATs.AssessementTasks = req.body.AssessementTasks;
-    if (req.body.InstructionalMaterials) Lessons.LessonInfo.InstructionalMaterials = req.body.InstructionalMaterials;
-    if (req.body.Remarks) Lessons.Remarks = req.body.Remarks;
-    if (req.body.Comment) Lessons.Comment = req.body.Comment;
+    if (req.body.GradingPeriod) FoundLesson.GradingPeriod = req.body.GradingPeriod;
+    if (req.body.Week) FoundLesson.Week = req.body.Week;
+    if (req.body.NoOfHours) FoundLesson.Week = req.body.NoOfHours;
+    if (req.body.Laboratory) FoundLesson.Laboratory = req.body.Laboratory;
+    if (req.body.Lecture) FoundLesson.Lecture = req.body.Lecture;
+    if (req.body.Title) FoundLesson.LessonInfo.Title = req.body.Title;
+    if (req.body.IntendedLearningOutcomes) FoundLesson.LessonInfo.InstructionalMaterials = req.body.IntendedLearningOutcomes;
+    if (req.body.Topics) FoundLesson.LessonInfo.Topics = req.body.Topics;
+    if (req.body.TeachingandLearningActivities) FoundLesson.LessonInfo.TLAandATs.TLA.TeachingandLearningActivities = req.body.TeachingandLearningActivities;
+    if (req.body.AssessementTasks) FoundLesson.LessonInfo.TLAandATs.ATs.AssessementTasks = req.body.AssessementTasks;
+    if (req.body.InstructionalMaterials) FoundLesson.LessonInfo.InstructionalMaterials = req.body.InstructionalMaterials;
+    if (req.body.Remarks) FoundLesson.Remarks = req.body.Remarks;
+    if (req.body.Comment) FoundLesson.Comment = req.body.Comment;
 
     //====================================================================================================
     //create function named filteredArray to callout prof database prof and filter prof ID and set to Interger
     const filteredArray = LessonFileDB.Lesson.filter(Lessons => Lessons.id !== parseInt(req.body.id));
 
     //Construct UnsortedArray 
-    const unsortedArray = [...filteredArray, LessonFileDB.Lesson];
+    const unsortedArray = [...filteredArray, FoundLesson ];
 
   LessonFileDB.setLesson(unsortedArray.sort((a, b) => a.id > b.id ? 1 :a.id < b.id ? -1 : 0 ));
     res.json(LessonFileDB.Lesson);
--- Controller/UpdateUserController.js
+++ Controller/UpdateUserController.js
@@ -13,30 +13,33 @@ const ProfDB = {
 const UpdateUserController = async(req, res) =>{
     
     //Creating a variable Profs with a function to find for professors ID
-    const Profs = ProfDB.Prof.find( Prof => Prof.id === parseInt(req.body.id));
+    const FoundProf = ProfDB.Prof.find( Prof => Prof.id === parseInt(req.body.id));
 
     //if Profs not found
-    if(!Profs){
+    if(!FoundProf){
         
         //send status 400 error and send message 
         return res.status(400).json({"message":`Professor ID ${req.body.id} not found`});
     }
     //====================================================================================================
     //request input for data below to change and to be updated
-    if (req.body.Firstname) Profs.Firstname = req.body.Firstname;
-    if (req.body.Lastname) Profs.Lastname = req.body.Lastname;
-    if (req.body.Roles) Profs.Roles = req.body.Roles;
-    if (req.body.Password) Profs.Password = req.body.Password
+    if (req.body.Firstname) FoundProf.Firstname = req.body.Firstname;
+    if (req.body.Lastname) FoundProf.Lastname = req.body.Lastname;
+    if (req.body.Roles) FoundProf.Roles = req.body.Roles;
+    if (req.body.Password) FoundProf.Password = req.body.Password
     //====================================================================================================
     //create function named filteredArray to callout prof database prof and filter prof ID and set to Interger
-    const filteredArray = ProfDB.Prof.filter(Profs => Profs.id !== parseInt(req.body.id));
+    const filteredArray = ProfDB.Prof.filter(Profs => Profs.id !== FoundProf.id);
 
     //Construct UnsortedArray 
-    const unsortedArray = [...filteredArray, ProfDB.Prof];
+    const unsortedArray = [...filteredArray, FoundProf];
 
-  ProfDB.setProf(unsortedArray.sort((a, b) => a.id > b.id ? 1 :a.id < b.id ? -1 : 0 ));
-    res.json(ProfDB.Prof);
+    ProfDB.setProf(unsortedArray.sort((a, b) => a.id > b.id ? 1 :a.id < b.id ? -1 : 0 ));
+
+    
     await fsPromises.writeFile(path.join(__dirname,'..','Models','Professor.json'), JSON.stringify(ProfDB.Prof)); 
+
+    res.json(ProfDB.Prof);
 }
 
 module.exports = {UpdateUserController}
